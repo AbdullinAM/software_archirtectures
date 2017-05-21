610746d010c57cda56fa75371476603c56c5df0d
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2017-05-21 11:21:21 +0000

    Drop all database in datagetaway

--- project/src/main/java/com/spbpu/storage/DataGateway.java
+++ project/src/main/java/com/spbpu/storage/DataGateway.java
@@ -6,7 +6,13 @@ package com.spbpu.storage;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 /**
  * Created by Admin on 21.04.2016.
@@ -42,6 +48,51 @@ public class DataGateway {
 
     public MysqlDataSource getDataSource() {
         return dataSource;
+    }
+
+    public void dropAll() throws SQLException {
+        executeSqlScript(getDataSource().getConnection(), new File("/home/kivi/workspace/software_archirtectures/project/db/db_create.sql"));
+    }
+
+    private void executeSqlScript(Connection conn, File inputFile) {
+
+        // Delimiter
+        String delimiter = ";";
+
+        // Create scanner
+        Scanner scanner;
+        try {
+            scanner = new Scanner(inputFile).useDelimiter(delimiter);
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+            return;
+        }
+
+        // Loop through the SQL file statements
+        Statement currentStatement = null;
+        while(scanner.hasNext()) {
+
+            // Get statement
+            String rawStatement = scanner.next() + delimiter;
+            try {
+                // Execute statement
+                currentStatement = conn.createStatement();
+                currentStatement.execute(rawStatement);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                // Release resources
+                if (currentStatement != null) {
+                    try {
+                        currentStatement.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+                currentStatement = null;
+            }
+        }
+        scanner.close();
     }
 
 }
